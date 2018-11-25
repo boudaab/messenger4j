735a37f359ecf86d9c735a23d758d181db7efd5d
Boudalh <boudalh@yesco.fr>
2018-11-25 22:46:16 +0100

    suppresion package mockito car unuse

--- src/main/java/com/github/messenger4j/quickstart/boot/SendController.java
+++ src/main/java/com/github/messenger4j/quickstart/boot/SendController.java
@@ -15,11 +15,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.mockito.internal.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
